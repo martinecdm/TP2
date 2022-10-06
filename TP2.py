f98f217aad431c4418067b7901708cf449bcecf1
martinecdm <martine@educ.college-montreal.qc.ca>
2022-10-06 16:16:01 -0400

    commit

--- TP2.py
+++ TP2.py
@@ -1,18 +1,33 @@
 import random
 
-nbr = random.randint(0,100)
+tri = 0
 var = "?"
-while True:
-    var = input("Entrez un nombre")
-    var = int(var)
-    if var < nbr :
-        aide = "trop bas"
-        print(var, aide)
 
-    else :
-        aide = "trop haut"
-        print(var, aide)
+
+while True:
+    f1 = int(input("choisis le nombre minimum"))
+    f2 = int(input("choisis le nombre maximum"))
+    nbr = random.randint(f1, f2)
+    print("j'ai choisi un nombre entre"f1" et"f2", devine le")
+    var = int(input("Entrez un nombre"))
+
+    if var < nbr :
+        print("le nombre est plus haut que ", var)
+        tri = tri + 1
+    elif var > nbr:
+        print("le nombre est plus bas que", var)
+        tri = tri + 1
     if var == nbr:
-        aide = "bravo !"
-        print(var, aide)
-        break
+        print("bravo le nombre était",nbr)
+        print("vous avez essayer",tri,"fois")
+
+        q1 = input("voulez-vous recommencer oui/non")
+
+        if q1 == "oui":
+            pass
+            tri = 0
+
+        elif q1 == "non":
+            print("merci et aurevoir")
+            break
+
