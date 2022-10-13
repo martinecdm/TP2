2fdd73287ed98dd72e566d43bca82ddc034e9f9d
martinecdm <martine@educ.college-montreal.qc.ca>
2022-10-13 09:20:29 -0400

    `commit`

--- TP2.py
+++ TP2.py
@@ -1,33 +1,36 @@
 import random
 
-tri = 0
-var = "?"
+nb_essai = 0
+essaie_joueur = "?"
 
 
 while True:
     f1 = int(input("choisis le nombre minimum"))
     f2 = int(input("choisis le nombre maximum"))
     nbr = random.randint(f1, f2)
-    print("j'ai choisi un nombre entre"f1" et"f2", devine le")
-    var = int(input("Entrez un nombre"))
+    print(f"j'ai choisi un nombre entre {f1} et {f2} devine le")
+    essaie_joueur = int(input("Entrez un nombre"))
+    while True:
 
-    if var < nbr :
-        print("le nombre est plus haut que ", var)
-        tri = tri + 1
-    elif var > nbr:
-        print("le nombre est plus bas que", var)
-        tri = tri + 1
-    if var == nbr:
-        print("bravo le nombre était",nbr)
-        print("vous avez essayer",tri,"fois")
+        if essaie_joueur < nbr :
+            print("le nombre est plus haut que ", essaie_joueur)
+            nb_essai = nb_essai + 1
+        elif essaie_joueur > nbr:
+            print("le nombre est plus bas que", essaie_joueur)
+            nb_essai = nb_essai + 1
+        if essaie_joueur == nbr:
+            print("bravo le nombre était",nbr)
+            print("vous avez essayer", nb_essai, "fois")
 
-        q1 = input("voulez-vous recommencer oui/non")
+            q1 = input("voulez-vous recommencer oui/non")
 
-        if q1 == "oui":
-            pass
-            tri = 0
+            if q1 == "oui":
+                pass
+                nb_essai = 0
 
-        elif q1 == "non":
-            print("merci et aurevoir")
-            break
+            elif q1 == "non":
+                print("merci et aurevoir")
+                break
+
+
 
