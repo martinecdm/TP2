dbf71bf05ede460bc410c816b5aa842b593b713a
martinecdm <martine@educ.college-montreal.qc.ca>
2022-10-14 09:54:58 -0400

    commit

--- TP2.py
+++ TP2.py
@@ -1,36 +1,46 @@
 import random
 
-nb_essai = 0
+nb_essai = 1
 essaie_joueur = "?"
+f1 = int(input("choisis le nombre minimum"))
+f2 = int(input("choisis le nombre maximum"))
+nbr = random.randint(f1, f2)
+print(f"j'ai choisi un nombre entre {f1} et {f2} devine le")
 
 
 while True:
-    f1 = int(input("choisis le nombre minimum"))
-    f2 = int(input("choisis le nombre maximum"))
-    nbr = random.randint(f1, f2)
-    print(f"j'ai choisi un nombre entre {f1} et {f2} devine le")
     essaie_joueur = int(input("Entrez un nombre"))
-    while True:
 
-        if essaie_joueur < nbr :
-            print("le nombre est plus haut que ", essaie_joueur)
-            nb_essai = nb_essai + 1
-        elif essaie_joueur > nbr:
-            print("le nombre est plus bas que", essaie_joueur)
-            nb_essai = nb_essai + 1
-        if essaie_joueur == nbr:
-            print("bravo le nombre était",nbr)
-            print("vous avez essayer", nb_essai, "fois")
+    if essaie_joueur < nbr :
+        print("le nombre est plus haut que ", essaie_joueur)
+        nb_essai = nb_essai + 1
 
-            q1 = input("voulez-vous recommencer oui/non")
+    elif essaie_joueur > nbr:
+        print("le nombre est plus bas que", essaie_joueur)
+        nb_essai = nb_essai + 1
 
-            if q1 == "oui":
-                pass
-                nb_essai = 0
+    if essaie_joueur == nbr:
+        print("bravo le nombre était",nbr)
 
-            elif q1 == "non":
-                print("merci et aurevoir")
-                break
+        print("vous avez essayer", nb_essai, "fois")
+
+        q1 = input("voulez-vous recommencer oui/non")
+
+        if q1 == "oui":
+            pass
+            nb_essai = 1
+            essaie_joueur = "?"
+            f1 = int(input("choisis le nombre minimum"))
+            f2 = int(input("choisis le nombre maximum"))
+            nbr = random.randint(f1, f2)
+            print(f"j'ai choisi un nombre entre {f1} et {f2} devine le")
+
+
+        elif q1 == "non":
+            print("merci et aurevoir")
+            break
+
+
 
 
 
